4103cf4ea1ecd4d09df4c08887d4549b3e061f64
Daniel01Cr <marinld03@gmail.com>
2023-10-18 15:01:30 -0600

    Cambios en Login, aun no funciona - DM

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoProgra5.Controllers;
+using ProyectoProgra5.Data;
 using ProyectoProgra5.Models;
 using System.Diagnostics;
 
@@ -7,12 +8,38 @@ namespace ProyectoGrupo5.Controllers
 {
     public class LoginController : Controller
     {
-        private readonly ILogger<LoginController> _logger;
+        private AppDbContext _db;
 
-        public LoginController(ILogger<LoginController> logger)
+        public LoginController(AppDbContext db)
         {
-            _logger = logger;
+            _db=db;
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registrarse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registrarse(Usuarios u)
+        {
+
+            u.IdRol=2;
+
+            if (!ModelState.IsValid) { return View(u); }
+
+            _db.Usuarios.Add(u);
+            _db.SaveChanges();
+
+            TempData["Exito"] = "Usuario creado ok";
+            return RedirectToAction("Login");
+        }
     }
 }
