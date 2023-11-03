f557f3810adad8fe9412fe5cf1faf7e5900b8077
Daniel01Cr <marinld03@gmail.com>
2023-11-03 00:52:34 -0600

    Capchat Funcional en loggin - DM, Con mucho gusto reyes

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ProyectoGrupo5.Models;
-using ProyectoProgra5.Controllers;
 using ProyectoProgra5.Data;
 using ProyectoProgra5.Models;
-using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json;
+
 
 namespace ProyectoGrupo5.Controllers
 {
@@ -29,25 +28,40 @@ namespace ProyectoGrupo5.Controllers
         }
         public IActionResult RevisarLogin(string Usuario,string Clave)
         {
-            List<Usuarios> Login = _db.Usuarios.Where(tp => tp.Correo == Usuario && tp.Contraseña == Clave).Include(v => v.Rol).ToList();
-            
-            if (Login != null)
+            var response = Request.Form["g-recaptcha-response"];
+            var secretKey = "6LdNn-4oAAAAANK0M29WuWIcF0I70d5rUzlvWSPU"; // Reemplaza con tu clave secreta reCAPTCHA
+            var client = new WebClient();
+
+            // Realiza la verificación del CAPTCHA con Google
+            var result = client.DownloadString($"https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={response}");
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(result);
+
+            if (captchaResponse.Success)
             {
-                foreach (var item in Login)
+                List<Usuarios> Login = _db.Usuarios.Where(tp => tp.Correo == Usuario && tp.Contraseña == Clave).Include(v => v.Rol).ToList();
+
+                if (Login != null)
                 {
-                    HttpContext.Session.SetString("Login", "True");
-                    HttpContext.Session.SetInt32("IdUsuario",item.Id);
-                    HttpContext.Session.SetString("Usuario", item.Nombre);
-                    HttpContext.Session.SetString("Correo", item.Correo);
-                    HttpContext.Session.SetString("Rol", item.Rol.Nombre);
+                    foreach (var item in Login)
+                    {
+                        HttpContext.Session.SetString("Login", "True");
+                        HttpContext.Session.SetInt32("IdUsuario", item.Id);
+                        HttpContext.Session.SetString("Usuario", item.Nombre);
+                        HttpContext.Session.SetString("Correo", item.Correo);
+                        HttpContext.Session.SetString("Rol", item.Rol.Nombre);
+                    }
+                    return RedirectToAction("Index", "Home");
                 }
-
-
-                return RedirectToAction("Index","Home");
+                else
+                {
+                    return RedirectToAction("Login");
+                }
             }
-            else {
-                return RedirectToAction("Login");
-            }
+            else
+            {
+                ModelState.AddModelError("Captcha", "Por favor, completa el CAPTCHA correctamente.");
+                return View("Login");
+            }            
         }
 
         public IActionResult Registrarse()
@@ -59,15 +73,37 @@ namespace ProyectoGrupo5.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Registrarse(Usuarios u)
         {
-            // Buscar el rol con Id igual a 2 en la base de datos
-            Rol rol = _db.Roles.FirstOrDefault(r => r.Id == 2);
-            u.Rol = rol;
-            _db.Usuarios.Add(u);
-            _db.SaveChanges();
-            TempData["Exito"] = "Usuario creado ok";
-            return RedirectToAction("Login");
+            var response = Request.Form["g-recaptcha-response"];
+            var secretKey = "6LdNn-4oAAAAANK0M29WuWIcF0I70d5rUzlvWSPU"; // Reemplaza con tu clave secreta reCAPTCHA
+            var client = new WebClient();
 
+            // Realiza la verificación del CAPTCHA con Google
+            var result = client.DownloadString($"https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={response}");
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(result);
+
+            if (captchaResponse.Success)
+            {
+                // El CAPTCHA es válido, continúa con el registro del usuario
+                // Buscar el rol con Id igual a 2 en la base de datos
+                Rol rol = _db.Roles.FirstOrDefault(r => r.Id == 2);
+                u.Rol = rol;
+                _db.Usuarios.Add(u);
+                _db.SaveChanges();
+                TempData["Exito"] = "Usuario creado ok";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                ModelState.AddModelError("Captcha", "Por favor, completa el CAPTCHA correctamente.");
+                return View(u);
+            }
+          
         }
 
+
     }
+}
+public class CaptchaResponse
+{
+    public bool Success { get; set; }
 }
